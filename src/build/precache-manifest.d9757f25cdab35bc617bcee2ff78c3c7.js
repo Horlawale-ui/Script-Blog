f751a74ea71ba363c820a5185cc2d31764db68dc
Horlawale-ui <ajayiolawale40@ymail.com>
2020-04-26 00:03:18 +0100

    firstcommit

---
+++ src/build/precache-manifest.d9757f25cdab35bc617bcee2ff78c3c7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "089f9ffbe1363e30f3a894dc4480be40",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d2da35198a6c4177f137",
+    "url": "/static/css/main.4a9d094a.chunk.css"
+  },
+  {
+    "revision": "dc545c27ae1a54f1a242",
+    "url": "/static/js/2.5c4680b5.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.5c4680b5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d2da35198a6c4177f137",
+    "url": "/static/js/main.e3920477.chunk.js"
+  },
+  {
+    "revision": "e988c064c70788dbc50e",
+    "url": "/static/js/runtime-main.a56884a5.js"
+  }
+]);
